41a0dec14441fd073dccdd203b1763e58b2a0187
xfilesadmin <ecarroll@uninorte.edu.co>
2021-10-27 15:56:17 -0500

    Edward

--- app.py
+++ app.py
@@ -19,6 +19,22 @@ def iniciar():
 def registro():
     return render_template('registro.html')
 
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/infodocente')
+def infodocente():
+    return render_template('info_docente.html')
+
+@app.route('/creactividad')
+def creactividad():
+    return render_template('creacion_actividad.html')
+
+@app.route('/detactividad')
+def detactividad():
+    return render_template('detalle_actividad.html')
+
 if __name__ == '__main__':
     app.jinja_env.auto_reload = True
     app.config['TEMPLATES_AUTO_RELOAD'] = True
